911c425bc2111989a6338b3efc3638cd2960c1b9
Michal Srb <michal@redhat.com>
2018-01-19 08:47:07 +0100

    Don't print number of hits.

--- src/main/java/com/redhat/fabric8/analytics/cpe2pkg/Main.java
+++ src/main/java/com/redhat/fabric8/analytics/cpe2pkg/Main.java
@@ -69,7 +69,6 @@ public class Main {
         TopDocs docs = searcher.search(q, 10);
         ScoreDoc[] hits = docs.scoreDocs;
 
-        System.out.println("Found " + hits.length + " hits.");
         for (int i = 0; i < hits.length; ++i) {
             int docId = hits[i].doc;
             Document d = searcher.doc(docId);
